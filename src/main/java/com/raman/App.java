343ed21d8ea9aa870324b88d225e20769da30440
aragiris <67544528+aragiris@users.noreply.github.com>
2020-10-31 21:46:40 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello from auto trigger 1" );
+        System.out.println( "Hello from auto trigger 2" );
     }
 }
