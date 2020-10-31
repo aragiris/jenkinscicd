01821911b3d2725a535667c43a782155c5661f8a
aragiris <67544528+aragiris@users.noreply.github.com>
2020-10-31 19:39:18 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello From Maven and Github" );
+        System.out.println( "Hello From Singleshot Maven" );
     }
 }
