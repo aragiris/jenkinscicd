e4179fbdc1e7d48410f10538f28abc0676cf200d
aragiris <67544528+aragiris@users.noreply.github.com>
2020-12-07 21:24:11 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello github and maven 1" );
+        System.out.println( "New Maven Project" );
     }
 }
